51e68996da02a5a3c53955c483cc5b2b6a840694
Anton Erholt <anton.erholt@gmail.com>
2018-03-16 18:26:26 +0100

    Fix missing sign in table

--- display.py
+++ display.py
@@ -8,12 +8,12 @@ def hdr(h):
 def table(f, xs):
     d = 5
     ds = str(d)
-    row = '|  {:.' + ds + 'f} ' + \
-          '|  {:.' + ds + 'f} |'
+    row = '|  {:+.' + ds + 'f} ' + \
+          '|  {:+.' + ds + 'f} |'
           
-    print('|   {0}x |{0}f(x) |'\
+    print('|    {0}x | {0}f(x) |'\
     	    .format(d * ' '))
-    print('+----{0}-+----{0}-+'\
+    print('+-----{0}-+-----{0}-+'\
     	    .format(d * '-'))
     for x in xs:
         print(row.format(x, f(x)))
@@ -21,7 +21,7 @@ def table(f, xs):
 
 def main():
     hdr('pretty table')
-    table(lambda x: x**2, [0, 1, 3])
+    table(lambda x: x, [-1, 0, 1, 3])
 
 
 if __name__ == '__main__':
