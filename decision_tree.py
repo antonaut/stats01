dfbfa9d24313f9212d4c012d339fbd8e8a69f690
Anton Erholt <anton.erholt@gmail.com>
2018-04-02 06:43:00 +0200

    closure for entropy function

--- decision_tree.py
+++ decision_tree.py
@@ -8,12 +8,23 @@ def unused(attr):
     return attr in used
 
 
-def entropy(attr):
-    """TODO FIX THIS"""
-    return math.sqrt(attr)
+def build_entropy(S, attrs):
+    S = S
+    attrs = attrs
+
+    def entropy(attr):
+        def p(z):
+            return float(len(attrs[z].nodes())) \
+            / float(len(S))
+
+        inner = lambda z: -p(z) * math.log2(p(z))
+        return sum(map(inner, attrs[attr].nodes()))
+
+    return entropy
 
 
 def id3(S, attributes):
+    entropy = build_entropy(S, attributes)
     ent = map(entropy, unused(attributes))
     smallest = reduce(lambda x, y: x < y, ent)
     pass
