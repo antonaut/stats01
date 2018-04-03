201e6fdd1e7d4d2f52e068afd48158f181827682
Anton Erholt <anton.erholt@gmail.com>
2018-04-03 06:41:31 +0200

    Add wf99 test data

--- decision_tree.py
+++ decision_tree.py
@@ -1,5 +1,7 @@
 import math
 from functools import reduce
+from collections import namedtuple
+
 
 used = set()
 
@@ -28,3 +30,53 @@ def id3(S, attributes):
     ent = map(entropy, unused(attributes))
     smallest = reduce(lambda x, y: x < y, ent)
     pass
+
+
+# test data
+
+def test_set_wf99():
+    O = {'sunny':1, 
+         'overcast':2, 
+         'rainy':4}
+    T = {'hot': 1,
+         'mild': 2,
+         'cool': 4}
+    H = {'high': 1,
+         'normal': 2}   
+    W = {'true': 1, 'false': 0}
+    P = {'yes': 1, 'no': 0}
+    def n(outlook,
+          temperature,
+          humidity,
+          windy,
+          play):
+        return namedtuple(
+        	    outlook=outlook,
+        	    temperature=temperature,
+        	    humidity=humidity,
+        	    windy=windy,
+        	    play=play)
+  
+    S = set(
+    	  n(1, 1, 1, 0, 0),
+    	  n(1, 1, 1, 1, 0),
+    	  n(2, 1, 1, 0, 1),
+    	  n(4, 2, 1, 0, 1),
+    	  \
+    	  n(4, 4, 2, 0, 1),
+    	  n(4, 4, 2, 1, 0),
+    	  n(2, 4, 2, 1, 1),
+    	  n(1, 2, 1, 0, 0),
+    	  \
+    	  n(1, 4, 2, 0, 1),
+    	  n(4, 2, 2, 0, 1),
+    	  n(1, 2, 2, 1, 1),
+    	  n(2, 2, 1, 1, 1),
+    	  \
+    	  n(2, 1, 2, 0, 1),
+    	  n(4, 2, 1, 1, 0)   	  
+    	)
+     return (S, (O, T, H, W, P))
+
+def print_test_set():
+  pass
