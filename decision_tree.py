8ab3cbff05097efdbc8b0237d0d92111f27131c0
Anton Erholt <anton.erholt@gmail.com>
2018-04-03 16:46:57 +0200

    Print test data set

--- decision_tree.py
+++ decision_tree.py
@@ -35,29 +35,33 @@ def id3(S, attributes):
 # test data
 
 def test_set_wf99():
-    O = {'sunny':1, 
-         'overcast':2, 
-         'rainy':4}
-    T = {'hot': 1,
+    labels = {
+    	'outlook': 
+    	    {'sunny':1, 
+          'overcast':2, 
+          'rainy':4},
+    'temperature': 
+        {'hot': 1,
          'mild': 2,
-         'cool': 4}
-    H = {'high': 1,
-         'normal': 2}   
-    W = {'true': 1, 'false': 0}
-    P = {'yes': 1, 'no': 0}
-    def n(outlook,
-          temperature,
-          humidity,
-          windy,
-          play):
-        return namedtuple(
-        	    outlook=outlook,
-        	    temperature=temperature,
-        	    humidity=humidity,
-        	    windy=windy,
-        	    play=play)
+         'cool': 4},
+    'humidity': 
+        {'high': 1,
+         'normal': 2}, 
+    'windy': 
+        {'true': 1, 
+         'false': 0},
+    'play': 
+        {'yes': 1, 
+         'no': 0}}
+
+    n = namedtuple('n',
+        	    """outlook,
+        	    temperature,
+        	    humidity,
+        	    windy,
+        	    play""")
   
-    S = set(
+    S = set([
     	  n(1, 1, 1, 0, 0),
     	  n(1, 1, 1, 1, 0),
     	  n(2, 1, 1, 0, 1),
@@ -74,9 +78,36 @@ def test_set_wf99():
     	  n(2, 2, 1, 1, 1),
     	  \
     	  n(2, 1, 2, 0, 1),
-    	  n(4, 2, 1, 1, 0)   	  
-    	)
-     return (S, (O, T, H, W, P))
+    	  n(4, 2, 1, 1, 0)])
+    return (S, labels)
 
-def print_test_set():
-  pass
+
+def key_from_value(d,
+                   val, 
+                   default=None):
+    for k in d:
+        if d.get(k) == val:
+            return k
+    return default
+
+
+def print_test_set(hdr='wf99',\
+	   dset=test_set_wf99):
+    s, labels = dset()
+    print('test dataset - ' + hdr)
+    print('-------------------')
+    for row in s:
+        res = '('
+        for f in row._fields:
+            res += \
+              	key_from_value(   \
+              		labels.get(f),   \
+              	 getattr(row, f))
+            res += ', '
+        res = res[:-2]
+        res += ')'
+        print(res)    
+
+
+if __name__ == '__main__':
+    print_test_set()
