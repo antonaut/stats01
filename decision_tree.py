349cafcf947f6a46252cf3d1767b6063940ade80
Anton Erholt <anton.erholt@gmail.com>
2018-04-11 08:18:05 +0200

    id3 description from Wikipedia

--- decision_tree.py
+++ decision_tree.py
@@ -30,6 +30,49 @@ def build_entropy(ds):
 
 
 def id3(ds):
+    """
+ID3 (Examples, 
+	    Target_Attribute, 
+	    Attributes) 
+Create a root node for the tree 
+If all examples are positive, 
+  Return the single-node tree Root, 
+  with label = +. 
+If all examples are negative, 
+  Return the single-node tree Root, 
+  with label = -. 
+If number of predicting attributes
+  is empty
+  Return the single node tree Root,
+    with label = most common value 
+    of the target attribute in the 
+    examples. 
+Otherwise 
+  Begin A ← The Attribute that
+  best classifies examples.
+  Decision Tree attribute for Root = A. 
+
+For each possible value, vi, of A, 
+Add a new tree branch below Root,
+ corresponding to the test A = vi. 
+ 
+Let Examples(vi) be the subset 
+of examples that have the value vi 
+for A 
+
+If Examples(vi) is empty 
+Then below this new branch 
+  add a leaf node with 
+  label = most common target value 
+  in the examples 
+Else below this new branch add 
+  the subtree 
+  ID3 (Examples(vi), 
+  	  Target_Attribute, 
+  	  Attributes – {A}) 
+End 
+Return Root    
+    """
     S, labels = ds
     attributes = labels.keys()
     entropy = build_entropy(ds)  
